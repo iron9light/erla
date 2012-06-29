916cc066104018e08dcb14af66691abb7f28d540
iron9light <iron9light@gmail.com>
2012-06-29 16:59:38 +0800

    Add Erla trait

--- src/main/scala/iron9light/erla/akka/ErlActor.scala
+++ src/main/scala/iron9light/erla/akka/ErlActor.scala
@@ -8,36 +8,19 @@ import akka.actor.{Stash, Actor}
  * @author il
  */
 
-trait ErlActor extends Actor with Stash {
-  def react[T](handler: PartialFunction[Any, T]): T@suspendable = {
+trait Erla {this: Actor with Stash =>
+  def react[T](hander: PartialFunction[Any, T]): T@suspendable = {
     shift[T, Unit, Unit] {
       cont: (T => Unit) => {
         context.become {
-          case msg if handler.isDefinedAt(msg) =>
+          case msg if hander.isDefinedAt(msg) =>
             unstashAll()
-            cont(handler(msg))
+            cont(hander(msg))
           case _ =>
             stash()
         }
       }
     }
-  }
-
-  def act(): Unit@suspendable
-
-  private object Spawn
-
-  self ! Spawn
-
-  def receive = {
-    case `Spawn` =>
-      reset[Unit, Unit] {
-        unstashAll()
-        act()
-        context.stop(self)
-      }
-    case _ =>
-      stash()
   }
 
   def await[T](future: Future[T]): T@suspendable = {
@@ -53,10 +36,44 @@ trait ErlActor extends Actor with Stash {
         }
         react {
           case (`o`, Right(x: T)) =>
-            x
+            () => x
           case (`o`, Left(e: Throwable)) =>
-            throw e
-        }
+            () => throw e
+        }.apply()
     }
+  }
+
+  def erlAct(act: => Unit@suspendable) {
+    reset[Unit, Unit] {
+      context.become(Map.empty, false)
+      act
+      context.unbecome()
+    }
+  }
+}
+
+trait ErlActor extends Actor with Stash with Erla {
+  private[this] var autoStop = true
+
+  def act(): Unit@suspendable
+
+  private object Spawn
+
+  self ! Spawn
+
+  def receive = {
+    case `Spawn` =>
+      reset[Unit, Unit] {
+        unstashAll()
+        act()
+        if (autoStop) context.stop(self)
+      }
+    case _ =>
+      stash()
+  }
+
+  def serve(handler: Receive) {
+    autoStop = false
+    context.become(handler)
   }
 }
